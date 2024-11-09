a2247a879e2c3ccbc46f4fbf6c4943958022dcc9
indinovia <indinovia.xpplg2@gmail.com>
2024-11-09 08:31:59 +0700

    UserAccountDrawerHeader

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -7,76 +7,158 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            title,
-            style: TextStyle(color: Colors.white),
-          ),
-          backgroundColor: Colors.blue[900],
-          foregroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(
+          title,
+          style: TextStyle(color: Colors.white),
         ),
-        drawer: buildGroupDrawer(context),
-        body: GridView.count(
-          crossAxisCount: 2,
-          crossAxisSpacing: 10.0,
-          mainAxisSpacing: 10.0,
-          shrinkWrap: true,
-          children: List.generate(10, (index) {
-            return Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Container(
-                  height: 5.0,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20.0),
-                    color: Colors.blue[200],
-                  ),
-                  child: Text('Materi $index',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
-                      )),
-                ));
-          }),
-        ));
+        backgroundColor: Colors.blue[900],
+        foregroundColor: Colors.white,
+      ),
+      drawer: buildGroupDrawer(context),
+      body: Center(
+        child: Text('Pemrograman Mobile $title'),
+      ),
+    );
+    // body: GridView.count(
+    //   crossAxisCount: 2,
+    //   crossAxisSpacing: 10.0,
+    //   mainAxisSpacing: 10.0,
+    //   shrinkWrap: true,
+    //   children: List.generate(10, (index) {
+    //     return Padding(
+    //         padding: EdgeInsets.all(10.0),
+    //         child: Container(
+    //           height: 5.0,
+    //           decoration: BoxDecoration(
+    //             borderRadius: BorderRadius.circular(20.0),
+    //             color: Colors.blue[200],
+    //           ),
+    //           child: Text('Materi $index',
+    //               textAlign: TextAlign.center,
+    //               style: TextStyle(
+    //                 color: Colors.black,
+    //                 fontSize: 20,
+    //               )),
+    //         ));
+    //   }),
+    // ));
   }
 
   Widget buildGroupDrawer(BuildContext context) {
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
-        children: [
+        children: <Widget>[
           DrawerHeader(
             decoration: BoxDecoration(color: Colors.blue[900]),
-            child: Text('Indi Novia Permadani',
-            style: TextStyle(
-             color: Colors.white,
-             fontSize: 30,
+            child: UserAccountsDrawerHeader(
+              decoration: BoxDecoration(color: Colors.blue[900]
+              ),
+              accountName: const Text('Indi Novia Permadani',
+              style: TextStyle(
+                fontSize: 18.0,
+                ),
+              ),
+              accountEmail: const Text('indinoviap@gmail.com',
+              ),
+              currentAccountPictureSize: Size.square(40),
+              currentAccountPicture: CircleAvatar(backgroundColor: Colors.white,
+              child: Text('I',
+              style: TextStyle(
+                fontSize: 25.0,
+                color: Colors.blue,
+              ),
+              ),
               ),
             ),
+          //   child: Text(
+          //     'Indi Novia Permadani',
+          //     style: TextStyle(
+          //       color: Colors.white,
+          //       fontSize: 30,
+          //     ),
+          //   ),
+          // ),
           ),
-          ExpansionTile(
-            title: const Text('materi'),
-            children: [
-              ListTile(title: Text('materi 1'), onTap: () {Navigator.pop(context);},),
-               ListTile(title: Text('materi 2'), onTap: () {Navigator.pop(context);},),
-                ListTile(title: Text('materi 3'), onTap: () {Navigator.pop(context);},),
-                 ListTile(title: Text('materi 4'), onTap: () {Navigator.pop(context);},),
-                  ListTile(title: Text('materi 5'), onTap: () {Navigator.pop(context);},),
-            ],
-            ),
-            ExpansionTile(
-            title: const Text('Tugas'),
-            children: [
-              ListTile(title: Text('Tugas 1'), onTap: () {Navigator.pop(context);},),
-               ListTile(title: Text('Tugas 2'), onTap: () {Navigator.pop(context);},),
-                ListTile(title: Text('Tugas 3'), onTap: () {Navigator.pop(context);},),
-                 ListTile(title: Text('Tugas 4'), onTap: () {Navigator.pop(context);},),
-                  ListTile(title: Text('Tugas 5'), onTap: () {Navigator.pop(context);},),
-            ],
-             ) //memperlebar
-        ],
-      ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Indi'),
+            subtitle: Text('XII RPL 2'),
+            trailing: Icon(Icons.chevron_right),
+          )
+          // ExpansionTile(
+          //   title: const Text('materi'),
+          //   children: [
+          //     ListTile(
+          //       title: Text('materi 1'),
+          //       onTap: () {
+          //         Navigator.pop(context);
+          //       },
+          //     ),
+          //     ListTile(
+          //       title: Text('materi 2'),
+          //       onTap: () {
+          //         Navigator.pop(context);
+          //       },
+          //     ),
+          //     ListTile(
+          //       title: Text('materi 3'),
+          //       onTap: () {
+          //         Navigator.pop(context);
+          //       },
+          //     ),
+          //     ListTile(
+          //       title: Text('materi 4'),
+          //       onTap: () {
+          //         Navigator.pop(context);
+          //       },
+          //     ),
+          //     ListTile(
+          //       title: Text('materi 5'),
+          //       onTap: () {
+          //         Navigator.pop(context);
+          //       },
+          //     ),
+          //   ],
+          // ),
+          // ExpansionTile(
+          //   title: const Text('Tugas'),
+          //   children: [
+          //     ListTile(
+          //       title: Text('Tugas 1'),
+          //       onTap: () {
+          //         Navigator.pop(context);
+          //       },
+          //     ),
+          //     ListTile(
+          //       title: Text('Tugas 2'),
+          //       onTap: () {
+          //         Navigator.pop(context);
+          //       },
+          //     ),
+          //     ListTile(
+          //       title: Text('Tugas 3'),
+          //       onTap: () {
+          //         Navigator.pop(context);
+          //       },
+          //     ),
+          //     ListTile(
+          //       title: Text('Tugas 4'),
+          //       onTap: () {
+          //         Navigator.pop(context);
+          //       },
+          //     ),
+          //     ListTile(
+          //       title: Text('Tugas 5'),
+          //       onTap: () {
+          //         Navigator.pop(context);
+          //       },
+          //     ),
+          //   ],
+            //memperlebar
+        ]
+      )
     );
   }
 }
